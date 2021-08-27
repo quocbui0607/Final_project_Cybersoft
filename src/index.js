c8662e48912818b7296fd064127c74f9d12fd976
Quoc Bui <thekingdom3036@gmail.com>
2021-08-27 16:09:48 +0700

    Add packages

--- src/index.js
+++ src/index.js
@@ -3,12 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "bootstrap/dist/js/bootstrap.min.js";
+import "popper.js/dist/umd/popper.min.js";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "jquery/dist/jquery.min.js";
+
+import { createStore, applyMiddleware, compose } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import { BrowserRouter } from "react-router-dom";
+import rootReducer from './redux/reducers/rootReducer';
+
+const comboEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose();
+
+const store = createStore(rootReducer, comboEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
